21153adbdfc51449cb7157cfc649dbcfe6b4099f
Patryk Łuszczek <52531090+Wasloso@users.noreply.github.com>
2025-01-25 17:34:44 +0100

    added collections enum

---
+++ database/enums.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class Collections(str, Enum):
+    DRIVERS = "drivers"
+    EDITORS = "editors"
+    PASSENGERS = "passengers"
+    INSPECTORS = "inspectors"
+    INSPECTIONS = "inspections"
+    FINES = "fines"
+    VEHICLES = "vehicles"
+    LINES = "lines"
+    RIDES = "rides"
+    STOPS = "stops"
+    TICKET_TYPES = "ticket_types"
+    TICKETS = "tickets"
+    VEHICLES = "vehicles"
