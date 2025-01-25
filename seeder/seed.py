3d6ea18c85505c36b3b6a91a52407cb69a013548
Patryk Łuszczek <52531090+Wasloso@users.noreply.github.com>
2025-01-25 17:59:05 +0100

    changed typing and added check if collection exists

--- seeder/seed.py
+++ seeder/seed.py
@@ -2,51 +2,58 @@ from database.connection import MongoDB
 from database.enums import Collections
 from faker import Faker
 from models import *
+from pymongo.database import Database
 
 
-def seed_passengers(database: MongoDB, count: int):
+def seed_passengers(db: Database, count: int):
     raise NotImplementedError
 
 
-def seed_drivers(database: MongoDB, count: int):
+def seed_drivers(db: Database, count: int):
     raise NotImplementedError
 
 
-def seed_editors(database: MongoDB, count: int):
+def seed_editors(db: Database, count: int):
     raise NotImplementedError
 
 
-def seed_inspectors(database: MongoDB, count: int):
+def seed_inspectors(db: Database, count: int):
     raise NotImplementedError
 
 
-def seed_vehicles(database: MongoDB, count: int):
+def seed_vehicles(db: Database, count: int):
     raise NotImplementedError
 
 
-def seed_lines(database: MongoDB, count: int):
+def seed_lines(db: Database, count: int):
     raise NotImplementedError
 
 
-def seed_rides(database: MongoDB, count: int):
+def seed_rides(db: Database, count: int):
     raise NotImplementedError
 
 
-def seed_stops(database: MongoDB, count: int):
+def seed_stops(db: Database, count: int):
     raise NotImplementedError
 
 
-def seed_ticket_types(database: MongoDB, count: int):
+def seed_ticket_types(db: Database, count: int):
     raise NotImplementedError
 
 
-def seed_tickets(database: MongoDB, count: int):
+def seed_tickets(db: Database, count: int):
     raise NotImplementedError
 
 
-def seed_fines(database: MongoDB, count: int):
+def seed_fines(db: Database, count: int):
     raise NotImplementedError
 
 
-def seed_inspections(database: MongoDB, count: int):
+def seed_inspections(db: Database, count: int):
     raise NotImplementedError
+
+
+def __check_collection(db: Database, collection: Collections):
+    if collection not in db.list_collection_names():
+        print(f"Collection {collection} not found. Creating collection.")
+        db.create_collection(collection)
