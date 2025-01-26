eb7f610acbd831b68f318b7700cf97a36b2591b1
Patryk Łuszczek <52531090+Wasloso@users.noreply.github.com>
2025-01-26 12:38:45 +0100

    added passengers and drivers generation

--- seeder/seed.py
+++ seeder/seed.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from database.connection import MongoDB
 from database.enums import Collections
 from faker import Faker
@@ -11,13 +12,54 @@ faker = Faker(locale=["en_US", "pl_PL"])
 
 
 def seed_passengers(db: MongoDB, count: int):
-    passengers = db.passengers
-    raise NotImplementedError
+    passengers_collection = db.passengers
+    for _ in range(count):
+        name: str = faker.first_name()
+        surname: str = faker.last_name()
+        login: str = faker.user_name()
+        password: str = faker.password()
+        email: str = faker.email()
+        phone: str = "".join(
+            str(faker.random_number(1)) for _ in range(faker.random_int(9, 15))
+        )
+        contact = Contact(email=email, phone=phone if faker.boolean() else None)
+        passenger = Passenger(
+            name=name, surname=surname, login=login, password=password, contact=contact
+        )
+        result = passengers_collection.insert_one(passenger.model_dump())
 
 
 def seed_drivers(db: MongoDB, count: int):
-    editors = db.editors
-    raise NotImplementedError
+    import uuid
+
+    drivers = db.drivers
+    for _ in range(count):
+        name: str = faker.first_name()
+        surname: str = faker.last_name()
+        login: str = faker.user_name()
+        password: str = faker.password()
+        email: str = faker.email()
+        phone: str = "".join(
+            str(faker.random_number(1)) for _ in range(faker.random_int(9, 15))
+        )
+        contact = Contact(email=email, phone=phone if faker.boolean() else None)
+        id_license = str(uuid.uuid4())
+        issue_date = faker.date_this_century()
+        expiration_date = issue_date + timedelta(
+            days=365 * faker.random_int(min=5, max=25)
+        )
+        license = License(
+            id_license=id_license, issued_on=issue_date, expires_on=expiration_date
+        )
+        driver = Driver(
+            name=name,
+            surname=surname,
+            login=login,
+            password=password,
+            contact=contact,
+            license=license,
+        )
+        result = drivers(driver.model_dump())
 
 
 def seed_editors(db: MongoDB, count: int):
