94121ba534cac2333556d916d070dbe7982b2681
Patryk Łuszczek <52531090+Wasloso@users.noreply.github.com>
2025-01-26 14:56:06 +0100

    added config to populate all collections

--- main.py
+++ main.py
@@ -17,7 +17,24 @@ from seeder.seed import (
 
 
 def main():
-    client: MongoDB = MongoDB()
+    db: MongoDB = MongoDB()
+    seed_config = {
+        seed_vehicles: 10,
+        seed_passengers: 10,
+        seed_drivers: 10,
+        seed_editors: 10,
+        seed_inspectors: 10,
+        seed_stops: 10,
+        seed_lines: 10,
+        seed_rides: 10,
+        seed_ticket_types: 10,
+        seed_tickets: 10,
+        seed_inspections: 10,
+        seed_fines: 10,
+    }
+    for seed_function, count in seed_config.items():
+        if count > 0:
+            seed_function(db, count)
 
 
 if __name__ == "__main__":
